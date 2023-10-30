8b0e8fc1e4e077e3a03fa6bf50ecfb888bb9b319
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-10-30 11:47:29 +0900

    changing mode logo to title
    logo->title

--- Labs/Lecture13_Game_Framework/logo_mode.py
+++ Labs/Lecture13_Game_Framework/logo_mode.py
@@ -1,5 +1,7 @@
 from pico2d import load_image, clear_canvas, update_canvas, get_events, get_time
 import game_framework
+import title_mode
+
 
 def init():
     global image
@@ -17,7 +19,7 @@ def finish():
 def update():
     global running
     if get_time() - logo_start_time >= 2.0:
-        game_framework.quit()
+        game_framework.change_mode(title_mode)
     pass
 
 def draw():
--- Labs/Lecture13_Game_Framework/main.py
+++ Labs/Lecture13_Game_Framework/main.py
@@ -1,7 +1,7 @@
 import pico2d
 from pico2d import delay, close_canvas, open_canvas
-import logo_mode
-import title_mode as start_mode
+import logo_mode as start_mode
+# import title_mode as start_mode
 import game_framework
 
 open_canvas()
