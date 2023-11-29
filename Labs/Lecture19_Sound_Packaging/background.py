bc17aa405f4cd7cd088672a2f8dbd46799bab6cb
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-11-29 17:34:02 +0900

    repeating background bgm

--- Labs/Lecture19_Sound_Packaging/background.py
+++ Labs/Lecture19_Sound_Packaging/background.py
@@ -13,7 +13,9 @@ class FixedBackground:
         self.w = self.image.w
         self.h = self.image.h
         # fill here
-
+        self.bgm = load_music('football.mp3')
+        self.bgm.set_volume(32)
+        self.bgm.repeat_play()
 
     def draw(self):
         self.image.clip_draw_to_origin(self.window_left, self.window_bottom, self.cw, self.ch, 0, 0)
