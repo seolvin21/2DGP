95734ae2f1310e7d2597cd1e75bf827cb388b550
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-11-29 18:31:04 +0900

    packaging

--- Labs/Lecture19_Sound_Packaging/ball.py
+++ Labs/Lecture19_Sound_Packaging/ball.py
@@ -17,6 +17,12 @@ class Ball:
         self.y = y if y else random.randint(100, server.background.h - 100)
 
         # fill here
+        if not Ball.boy_eat_sound:
+            Ball.zombie_eat_sound = load_wav('zombie_pickup.wav')
+            Ball.zombie_eat_sound.set_volume(32)
+            Ball.boy_eat_sound = load_wav('pickup.wav')
+            Ball.boy_eat_sound.set_volume(32)
+
 
     def draw(self):
         self.image.draw(self.x - server.background.window_left, self.y - server.background.window_bottom)
@@ -29,6 +35,11 @@ class Ball:
 
     def handle_collision(self, group, other):
         match group:
+            case 'zombie:ball':
+                # fill here
+                Ball.zombie_eat_sound.play()
+                game_world.remove_object(self)
             case 'boy:ball':
                 # fill here
+                Ball.boy_eat_sound.play()
                 game_world.remove_object(self)
